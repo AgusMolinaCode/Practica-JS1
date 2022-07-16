fab84d42142a919bede793f72fedc497675c5d95
Agus_Molina <am.motos@hotmail.com>
2022-07-16 14:13:45 -0300

    1.7

--- script.js
+++ script.js
@@ -2,20 +2,21 @@ let redBtn = document.getElementById("red")
 let blackBtn = document.getElementById("black")
 let stockBtn = document.getElementById("stock")
 let whiteBtn = document.getElementById("white")
-let imagen = document.getElementById("imagen")
+let imgchange = document.getElementById("imagen")
 
 redBtn.onclick = function(){
-    imagen.style.backgroundImage = "url(IMAGENES/X003-Y015.jpg)";
+    imgchange.src = "IMAGENES/X003-Y015.jpg";
 }
 
 blackBtn.onclick = function(){
-    imagen.style.backgroundImage = "url(IMAGENES/X001-Y015.jpg)";
+    imgchange.src = "IMAGENES/X001-Y015.jpg";
 }
 
 stockBtn.onclick = function(){
-    imagen.style.backgroundImage = "url(IMAGENES/X004-Y015.jpg)";
+    imgchange.src = "IMAGENES/X004-Y015.jpg";
 }
 
 whiteBtn.onclick = function(){
-    imagen.style.backgroundImage = "url(IMAGENES/X002-Y015.jpg)";
+    imgchange.src = "IMAGENES/X002-Y015.jpg";
 }
+
